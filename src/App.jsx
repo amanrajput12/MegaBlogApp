7fe61353de6cd7339586ba33c2173cdd53a07198
Aman Rajput <rajputaman724857@gmail.com>
2024-03-02 11:41:47 +0530

    setup redux store

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,41 @@
-import { useState } from 'react'
-
+import { useEffect, useState } from 'react'
+import {useDispatch} from "react-redux"
 import './App.css'
-
+import authService from "./Appwrite/auth"
+import { login, logout } from './store/authSlice'
+import { Footer, Header } from './components/Index'
 function App() {
-  const [count, setCount] = useState(0)
-console.log("ev file data",import.meta.env.VITE_URL);
-  return (
-    <>
-     <h2>Hello</h2>
-    </>
-  )
+  const [loading, setLoading] = useState(true)
+   const dispatch = useDispatch()
+
+    useEffect(()=>{
+    authService.getCurrentUser()
+    .then((userData)=>{
+      if(userData){
+        dispatch(login(userData))
+      }
+      else{
+        dispatch(logout())
+      }
+    })
+    .catch((error)=>
+    console.log("error to fetch the detail",error)
+    )
+    .finally(
+      ()=>setLoading(false)
+    )
+   },[])
+console.log("ev file data");
+  return !loading ? (
+   <div className='min-h-screen flex flex-wrap content-between bg-gray-400'>
+    <div className='w-full block'>
+       <Header/>
+       <Footer/>
+    </div>
+
+   </div>
+  ):
+  null
 }
 
 export default App
